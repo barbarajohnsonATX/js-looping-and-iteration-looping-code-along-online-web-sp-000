39d4b48e28c24ba085fec6294250ef30fa125d89
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-20 23:16:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Code your solutions in this file
+ function wrapGifts(gifts) 
+ {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
  
+  return gifts;
+}
