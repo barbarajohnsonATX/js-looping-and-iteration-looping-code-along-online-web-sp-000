f1a0d95d27d0c2543a63d490284a5105d186355f
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-20 23:19:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solutions in this file
  function wrapGifts(gifts) 
  {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`'Thank you, ${gifts[i]}, for the wonderful surprise gift!);
-  }
+   for (let i = 0; i < gifts.length; i++) 
+   {
+    console.log("Thank you, ${gifts[i]}, for the wonderful surprise gift!");
+   }
  
   return gifts;
 }
